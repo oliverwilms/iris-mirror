5f4638714b0484b99149e6e8e8ac46bb7198380b
Oliver Wilms <50807396+oliverwilms@users.noreply.github.com>
2021-08-19 10:41:21 -0500

    https://notearena.com/lesson/file-manipulation-in-linux-shell-scripting/

---
+++ writer.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+COUNT=1
+# create a file
+touch output.txt
+
+#Internal Field Separator (IFS) that is used for word splitting 
+#after expansion and to split lines into words with the read builtin command
+while IFS='' read -r LINE
+do
+  echo "LINE $COUNT: $LINE" >> output.txt
+  ((COUNT++))
+#$1 gets the file passed a parameter while running the script
+done < "$1"
+
+exit 0
