a7931c625567bd3ce0a6b4d20f6c53b65074661a
Oliver Wilms <50807396+oliverwilms@users.noreply.github.com>
2021-08-19 10:47:40 -0500

    Update writer.sh

--- writer.sh
+++ writer.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 COUNT=1
+output=output.txt
 # create a file
-touch output.txt
+touch $output
 
 #Internal Field Separator (IFS) that is used for word splitting 
 #after expansion and to split lines into words with the read builtin command
 while IFS='' read -r LINE
 do
-  echo "LINE $COUNT: $LINE" >> output.txt
+  echo "LINE $COUNT: $LINE" >> $output
   ((COUNT++))
 #$1 gets the file passed a parameter while running the script
 done < "$1"
