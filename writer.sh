9fc6bee9ff257c08833a81ec0192bf5d083da5e1
Oliver Wilms <50807396+oliverwilms@users.noreply.github.com>
2021-08-19 19:50:16 -0500

    Update writer.sh

--- writer.sh
+++ writer.sh
@@ -1,16 +1,36 @@
 #!/usr/bin/env bash
-COUNT=1
+dummy=0
+flag=0
 output=output.txt
+empty=""
+pattern="?MapMirrors.IRISMIRROR?"
+next="?MirrorMember?"
 # create a file
+rm $output
 touch $output
 
 #Internal Field Separator (IFS) that is used for word splitting 
 #after expansion and to split lines into words with the read builtin command
 while IFS='' read -r LINE
 do
-  echo "LINE $COUNT: $LINE" >> $output
-  ((COUNT++))
-#$1 gets the file passed a parameter while running the script
+  match=0
+  newLine="" 
+  case $LINE in
+    $pattern) ((flag++));;
+    $next) ((flag++));;
+    "$empty") ((dummy++));;
+    *) ((match++));;
+  esac
+  if [ $flag -eq 1 ];
+  then
+    if [ $match -eq 1 ];
+    then
+      newLine=" **UPDATE";
+      LINE=$newLine;
+    fi
+  fi
+  echo "$newLine $LINE" >> $output
+#$1 gets the file passed as a parameter while running the script
 done < "$1"
 
 exit 0
